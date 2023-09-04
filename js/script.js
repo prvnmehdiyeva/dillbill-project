931ff97d22b06d49142b6dcc8573c1ebb4759e51
prvnmehdiyeva <parvinrm@code.edu.az>
2023-09-04 19:25:20 +0400

    json commit

--- js/script.js
+++ js/script.js
@@ -1,77 +1,78 @@
-const data = {
-  results: [
-    {
-      image: "images/person1.jpg",
-      name: "Jennifer 1",
-    },
-    {
-      image: "images/person2.jpg",
-      name: "Jennifer 2",
-    },
-    {
-      image: "images/person3.jpg",
-      name: "Jennifer 3",
-    },
-    {
-      image: "images/person4.jpg",
-      name: "Jennifer 4",
-    },
-    {
-      image: "images/person3.jpg",
-      name: "Jennifer 5",
-    },
-    {
-      image: "images/person2.jpg",
-      name: "Jennifer 6",
-    },
-  ],
-};
+// const data = {
+//   results: [
+//     {
+//       image: "images/person1.jpg",
+//       name: "Jennifer 1",
+//     },
+//     {
+//       image: "images/person2.jpg",
+//       name: "Jennifer 2",
+//     },
+//     {
+//       image: "images/person3.jpg",
+//       name: "Jennifer 3",
+//     },
+//     {
+//       image: "images/person4.jpg",
+//       name: "Jennifer 4",
+//     },
+//     {
+//       image: "images/person3.jpg",
+//       name: "Jennifer 5",
+//     },
+//     {
+//       image: "images/person2.jpg",
+//       name: "Jennifer 6",
+//     },
+//   ],
+// };
 // Wait for the DOM to be fully loaded before executing the code
 document.addEventListener("DOMContentLoaded", () => {
   // Smooth scroll functionality for navigation links
-  const links = document.querySelectorAll("nav ul li a");
+  const links = document.querySelectorAll("nav ul li a")
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetID = link.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetID);
+      e.preventDefault()
+      const targetID = link.getAttribute("href").substring(1)
+      const targetElement = document.getElementById(targetID)
       if (targetElement) {
-        targetElement.scrollIntoView({ behavior: "smooth" });
+        targetElement.scrollIntoView({ behavior: "smooth" })
       }
-    });
-  });
+    })
+  })
 
   // Toggle and show/hide functionality for card elements
-  const cards = document.querySelectorAll(".cards");
+  const cards = document.querySelectorAll(".cards")
   cards.forEach((firstCard) => {
-    const card = firstCard.querySelectorAll(".card");
+    const card = firstCard.querySelectorAll(".card")
     card.forEach((childCard) => {
       childCard.addEventListener("click", (e) => {
-        e.preventDefault();
-        childCard.classList.toggle("active");
-        const generalText = childCard.querySelector(".general-text");
+        e.preventDefault()
+        childCard.classList.toggle("active")
+        const generalText = childCard.querySelector(".general-text")
         if (generalText.style.display === "block") {
-          generalText.style.display = "none";
+          generalText.style.display = "none"
         } else {
-          generalText.style.display = "block";
+          generalText.style.display = "block"
         }
-      });
-    });
-  });
+      })
+    })
+  })
 
   // Display teacher information using Swiper.js
   async function displayTeachers() {
-    const teachers = data.results;
+    const response = await fetch("js/data.json")
+    const teachers = await response.json()
     teachers.forEach((result) => {
-      const div = document.createElement("div");
-      div.classList.add("swiper-slide");
+      const div = document.createElement("div")
+      div.classList.add("swiper-slide")
       div.innerHTML = `
         <img src="${result.image}" alt="" />
         <h3>${result.name}</h3>
-      `;
-      document.querySelector(".swiper-wrapper").appendChild(div);
-      initSwiper();
-    });
+      `
+      document.querySelector(".swiper-wrapper").appendChild(div)
+      initSwiper()
+    })
   }
 
   // Initialize Swiper.js for the teacher slider
@@ -96,20 +97,20 @@ document.addEventListener("DOMContentLoaded", () => {
           slidesPerView: 4,
         },
       },
-    });
+    })
   }
 
   // Display a pop-up when login or sign-up buttons are clicked
-  const buttons = document.querySelectorAll(".login, .sign-up");
+  const buttons = document.querySelectorAll(".login, .sign-up")
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      const popup = document.getElementById("pop-up");
-      popup.style.display = "grid";
-      popup.style.transition = "all 0.5s";
+      const popup = document.getElementById("pop-up")
+      popup.style.display = "grid"
+      popup.style.transition = "all 0.5s"
       document.querySelector(".icons").addEventListener("click", () => {
-        popup.style.display = "none";
-      });
-    });
-  });
-  displayTeachers();
-});
+        popup.style.display = "none"
+      })
+    })
+  })
+  displayTeachers()
+})
